6a2451546c101a41992fbb338fce70c8132869a6
Chris Hutchison <chris88@gmail.com>
2018-04-20 16:35:04 -0400

    Update spooler.py

--- isso/ext/spooler.py
+++ isso/ext/spooler.py
@@ -1,10 +1,104 @@
-def spooler(args):
-    try:
-        self._sendmail(args[b"subject"].decode("utf-8"),
-                       args["body"].decode("utf-8"))
-    except smtplib.SMTPConnectError:
-        return uwsgi.SPOOL_RETRY
-    else:
-        return uwsgi.SPOOL_OK
+
+
+
+class SMTP(object):
+
+    def __init__(self, isso):
+
+        self.isso = isso
+        self.conf = isso.conf.section("smtp")
+        gh = isso.conf.get("general", "host")
+        if type(gh) == str:
+            self.general_host = gh
+        #if gh is not a string then gh is a list
+        else:
+            self.general_host = gh[0]
+
+        # test SMTP connectivity
+        try:
+            with self:
+                logger.info("connected to SMTP server")
+        except (socket.error, smtplib.SMTPException):
+            logger.exception("unable to connect to SMTP server")
+
+        if uwsgi:
+            def spooler(args):
+                try:
+                    self._sendmail(args[b"subject"].decode("utf-8"),
+                                   args["body"].decode("utf-8"))
+                except smtplib.SMTPConnectError:
+                    return uwsgi.SPOOL_RETRY
+                else:
+                    return uwsgi.SPOOL_OK
+
+            uwsgi.spooler = spooler
+
+    def __enter__(self):
+        klass = (smtplib.SMTP_SSL if self.conf.get(
+            'security') == 'ssl' else smtplib.SMTP)
+        self.client = klass(host=self.conf.get('host'),
+                            port=self.conf.getint('port'),
+                            timeout=self.conf.getint('timeout'))
+
+        if self.conf.get('security') == 'starttls':
+            if sys.version_info >= (3, 4):
+                import ssl
+                self.client.starttls(context=ssl.create_default_context())
+            else:
+                self.client.starttls()
+
+        username = self.conf.get('username')
+        password = self.conf.get('password')
+        if username and password:
+            if PY2K:
+                username = username.encode('ascii')
+                password = password.encode('ascii')
+
+            self.client.login(username, password)
+
+        return self.client
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.client.quit()
+
+    def __iter__(self):
+        yield "comments.new:after-save", self.notify
+
+
+
+    def notify(self, thread, comment):
+
+        body = self.format(thread, comment)
+
+        if uwsgi:
+            uwsgi.spool({b"subject": thread["title"].encode("utf-8"),
+                         b"body": body.encode("utf-8")})
+        else:
+            start_new_thread(self._retry, (thread["title"], body))
+
+    def _sendmail(self, subject, body):
+
+        from_addr = self.conf.get("from")
+        to_addr = self.conf.get("to")
+
+        msg = MIMEText(body, 'plain', 'utf-8')
+        msg['From'] = from_addr
+        msg['To'] = to_addr
+        msg['Date'] = formatdate(localtime=True)
+        msg['Subject'] = Header(subject, 'utf-8')
+
+        with self as con:
+            con.sendmail(from_addr, to_addr, msg.as_string())
+
+    def _retry(self, subject, body):
+        for x in range(5):
+            try:
+                self._sendmail(subject, body)
+            except smtplib.SMTPConnectError:
+                time.sleep(60)
+            else:
+                break
+
+
 
 uwsgi.spooler = spooler
